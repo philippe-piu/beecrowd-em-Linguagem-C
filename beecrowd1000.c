ee3c1ecdefcf804f5d751d8749b6ae939f347f27
Philippe Lima <42221580+philippe-piu@users.noreply.github.com>
2023-03-09 14:46:53 -0300

    Add files via upload

---
+++ beecrowd1000.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+//beecrowd1000;
+int main(){
+
+	printf("Hello World!\n");
+
+	return(0);
+}
---
+++ beecrowd1001.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+//beecrowd 1001;
+int main(){
+
+	int a, b, resultado;
+
+	a = 0;
+	b = 0;
+	resultado = 0;
+
+	scanf("%d",&a);
+	scanf("%d",&b);
+
+	resultado = a + b;
+
+	printf("X = %d\n",resultado);
+
+	return(0);
+}
